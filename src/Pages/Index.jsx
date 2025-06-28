b04106f01ba544c0b36a868554c45e9510d2f354
Raheel Arfeen <raheelarfeen@gmail.com>
2025-06-28 16:24:49 +0600

    finished about section

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Components/Header";
 import Hero from "../Components/Hero";
+import About from "../Components/About";
 
 const Index = () => {
     return (
         <div className="min-h-screen bg-gray-900 text-white w-full">
             <Header/>
             <Hero/>
+            <About/>
         </div>
     );
 };
