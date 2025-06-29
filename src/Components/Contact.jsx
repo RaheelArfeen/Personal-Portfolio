e08b6317ea88563458126cbb623e8c90b1d40a87
Raheel Arfeen <raheelarfeen@gmail.com>
2025-06-29 13:48:39 +0600

    finished

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -104,7 +104,7 @@ const Contact = () => {
                     </p>
                 </motion.div>
 
-                <div className="max-w-6xl mx-auto">
+                <div className="max-w-7xl mx-auto">
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
                         {/* Contact Form */}
                         <motion.div
